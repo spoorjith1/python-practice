fe34e959f920fe578869ebabfd55c94be1f4519b
Spoorjith Malebalu <spoorjith1@gmail.com>
2026-02-17 20:07:29 +0530

    Improve error handling and output formatting

--- py/exception_handling.py
+++ py/exception_handling.py
@@ -1,11 +1,12 @@
+#ERROR Handling :
 while True:
     try:
         n1 = int(input("First num : "))
         n2 = int(input("Second num : "))
         n3 = n1/n2
-        print(n3)
+        print(f"{n1}/{n2} = {n3}")
         break
-    except ValueError:
+    except ValueError: #for input of strings
         print("*input num must be Interger")
-    except ZeroDivisionError:
+    except ZeroDivisionError: # stopping zero from dividing
         print("*cannot divide number with zero")
